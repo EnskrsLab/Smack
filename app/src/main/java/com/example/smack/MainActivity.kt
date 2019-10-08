25a32333df12792a85e04940034bf7501f925b25
EnskrsLab <eneskaras@hotmail.com>
2019-10-08 23:41:48 +0300

    Lesson Login Activity UI completed

--- app/src/main/java/com/example/smack/MainActivity.kt
+++ app/src/main/java/com/example/smack/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.smack
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
@@ -41,6 +42,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun loginBtnNavClicked(view: View){
+
+        val loginIntent = Intent(this,LoginActivity::class.java)
+        startActivity(loginIntent)
 
     }
 
