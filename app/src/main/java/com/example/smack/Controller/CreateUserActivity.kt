07801852dbe230cda9625e25ac811e6b486177a9
EnskrsLab <eneskaras@hotmail.com>
2019-11-27 22:03:32 +0300

    Lesson Add User completed

--- app/src/main/java/com/example/smack/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/example/smack/Controller/CreateUserActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import com.example.smack.R
 import com.example.smack.Services.AuthService
+import com.example.smack.Services.UserDataService
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
 
@@ -53,14 +54,22 @@ class CreateUserActivity : AppCompatActivity() {
 
     fun createUserClicked(view: View)
     {
+        val userName = createUserNameText.text.toString()
         val email = createEmailText.text.toString()
         val password = createPasswordText.text.toString()
         AuthService.registerUser(this, email, password){registerSuccess ->
             if (registerSuccess){
                 AuthService.loginUser(this, email, password){ loginSuccess ->
                     if (loginSuccess){
-                        println(AuthService.authToken)
-                        println(AuthService.userEmail)
+                        AuthService.createUser(this, userName, email, userAvatar, avatarColor) { createSucces ->
+                            if (createSucces){
+                                println(UserDataService.avatarName)
+                                println(UserDataService.avatarColor)
+                                println(UserDataService.name)
+                                finish()
+
+                            }
+                        }
                     }
                 }
 
